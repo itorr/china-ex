dd9b8fe82db89fc785f20a79ccf2bbb70c7610c6
yusancky <umbrellacky@qq.com>
2022-10-27 18:33:53 +0800

    格式化文件内容

--- 生成字体子集.js
+++ 生成字体子集.js
@@ -1,81 +1,88 @@
-const replaceSVG = text=>{
-    text = text.replace(/ transform="matrix\(1 0 0 1 (\d+) (\d+)\)" class=".+"/,' x="$1" y="$2"')
-    return text;
+const replaceSVG = (text) => {
+  text = text.replace(
+    / transform="matrix\(1 0 0 1 (\d+) (\d+)\)" class=".+"/,
+    ' x="$1" y="$2"'
+  );
+  return text;
 };
 
 let fontAPI = `https://lab.magiconch.com/api/fontmin`;
-const generateFontURL = (name,text)=>{
-    text += '0';
-    text = text.replace(/\s/g,'');
-    text = Array.from(new Set(text)).sort().join('');
+const generateFontURL = (name, text) => {
+  text += "0";
+  text = text.replace(/\s/g, "");
+  text = Array.from(new Set(text)).sort().join("");
 
-    if(!text) return requestAnimationFrame(onOver);
+  if (!text) return requestAnimationFrame(onOver);
 
-    console.log(text);
+  console.log(text);
 
-    const unicode = str2utf8(text).join();
-    const fontURL = `${fontAPI}?name=${name}&unicode=${unicode}&type=woff`;
+  const unicode = str2utf8(text).join();
+  const fontURL = `${fontAPI}?name=${name}&unicode=${unicode}&type=woff`;
 
-    return fontURL;
+  return fontURL;
 };
 
-const getFontFromText = (name,text,onOver=_=>{})=>{
-    const fontURL = generateFontURL(name,text);
-    loadFont(name,fontURL,_=>{
-        onOver(_)
-    })
+const getFontFromText = (name, text, onOver = (_) => {}) => {
+  const fontURL = generateFontURL(name, text);
+  loadFont(name, fontURL, (_) => {
+    onOver(_);
+  });
 };
 
-const loadFont = async (fontName,fontURL,callback) => {
-	const fontFace = new FontFace(fontName, `url(${fontURL})`);
-	fontFace.load().then(fontFace => {
-		document.fonts.add(fontFace);
-		callback(fontFace);
-	}).catch(e=>{
-        callback();
+const loadFont = async (fontName, fontURL, callback) => {
+  const fontFace = new FontFace(fontName, `url(${fontURL})`);
+  fontFace
+    .load()
+    .then((fontFace) => {
+      document.fonts.add(fontFace);
+      callback(fontFace);
     })
+    .catch((e) => {
+      callback();
+    });
 };
 function str2utf8(str) {
-    return str.split('').map(s=>s.charCodeAt(0))
+  return str.split("").map((s) => s.charCodeAt(0));
 }
 function utf82str(str) {
-    return String.fromCharCode.apply(null,Array.from(str))
-};
+  return String.fromCharCode.apply(null, Array.from(str));
+}
 
-const { readFileSync, writeFileSync } = require('fs');
-const getTextFromHTMLFile = (filename)=>{
-    let html = readFileSync(filename,'utf8');
-    const texts = [];
+const { readFileSync, writeFileSync } = require("fs");
+const getTextFromHTMLFile = (filename) => {
+  let html = readFileSync(filename, "utf8");
+  const texts = [];
 
-    const addTextFromMatch = (all,text)=>{
-        text = text.replace(/<.+?>/g,'');
+  const addTextFromMatch = (all, text) => {
+    text = text.replace(/<.+?>/g, "");
 
-        texts.push(text);
-        return '';
-    }
-    html = html.replace(/<(?:path|rect) id="(.+?)"/g,addTextFromMatch);
-    html = html.replace(/<text .+?>(.+?)<\/text>/g,addTextFromMatch);
-    html = html.replace(/<a .+?>(.+?)<\/a>/g,addTextFromMatch);
-    html = html.replace(/<h2 .+?>(.+?)<\/h2>/g,addTextFromMatch);
-    html = html.replace(/<p>([\s\S]+?)<\/p>/g,addTextFromMatch);
+    texts.push(text);
+    return "";
+  };
+  html = html.replace(/<(?:path|rect) id="(.+?)"/g, addTextFromMatch);
+  html = html.replace(/<text .+?>(.+?)<\/text>/g, addTextFromMatch);
+  html = html.replace(/<a .+?>(.+?)<\/a>/g, addTextFromMatch);
+  html = html.replace(/<h2 .+?>(.+?)<\/h2>/g, addTextFromMatch);
+  html = html.replace(/<p>([\s\S]+?)<\/p>/g, addTextFromMatch);
 
-    return texts.join('');
+  return texts.join("");
 };
 
 const defaultText = `1234567890:`;
-const text = defaultText + getTextFromHTMLFile('html/index.html') + getTextFromHTMLFile('china-ex.svg');
-const fontURL = generateFontURL(`JiaLiDaYuanJF`,text);
+const text =
+  defaultText +
+  getTextFromHTMLFile("html/index.html") +
+  getTextFromHTMLFile("china-ex.svg");
+const fontURL = generateFontURL(`JiaLiDaYuanJF`, text);
 
+const axios = require("axios");
 
-const axios = require('axios');
-
-
-const downFontFile = async _=>{
-    let r = await axios.get(fontURL,{
-        responseType: 'arraybuffer'
-    });
-    console.log(r.data);
-    writeFileSync('html/字体.woff',r.data,'binary');
+const downFontFile = async (_) => {
+  let r = await axios.get(fontURL, {
+    responseType: "arraybuffer",
+  });
+  console.log(r.data);
+  writeFileSync("html/字体.woff", r.data, "binary");
 };
 
 downFontFile();
